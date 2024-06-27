b10e103a527e478e2e941b56df73ba5d9b851146
Faika Mushtaq <faikamushtaq121@gmail.com>
2024-06-27 21:03:02 +0530

    update

---
+++ ques2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int hours;
+    int minutes;
+    printf("enter time in minutes:");
+    scanf("%d",minutes);
+    printf("total number of hours=%d",minutes/60);
+}
