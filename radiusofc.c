3a743325c9434a3fff0214e2c3e8f64438b10c40
Faika Mushtaq <faikamushtaq121@gmail.com>
2024-06-27 20:46:18 +0530

    update

---
+++ radiusofc.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+ int r;
+    printf("enter the radius of the sphere =");
+    scanf("%d",&r);
+    printf(" the volume of the sphere=%d","v=(4.0/3.0)*PI*(r*r*r)");
+}
