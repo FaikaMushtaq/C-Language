b852454bcc5d4b49a4e93c47b1312444842cccf7
Faika Mushtaq <faikamushtaq121@gmail.com>
2024-06-27 20:58:42 +0530

    update

---
+++ ifelse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int age;
+    printf("enter your age =");
+    scanf("%d",&age);
+
+    if(age>=18)
+    {
+        printf("the person is elligible for voting");
+    }
+  else{
+    printf("the person is not elligible for voting");
+  }
+
+}
+
+
