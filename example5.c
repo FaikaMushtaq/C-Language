d669d9d9c3f7da66bc032523c877a5c625b8c603
Faika Mushtaq <faikamushtaq121@gmail.com>
2024-06-27 20:56:32 +0530

    update

---
+++ example5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("enter a number");
+    scanf("%d",&number);
+
+    if(number>=1 && number<=1000){
+        printf("it is a natural number");
+    }
+
+        else {
+                printf("it is not a natural number");
+    }
+  
+   }
+        
+       
